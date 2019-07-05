793f6038ea5be68e74e66175c68288d1e200631e
guilk <guiliangke@gmail.com>
2019-07-05 17:59:50 -0400

    msrvtt data list

--- collect_msrvtt_dataset.py
+++ collect_msrvtt_dataset.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
 
     root_path = '/mnt/sda/AVS/MSR-VTT/'
     videos = os.listdir(os.path.join(root_path, 'TrainValVideo'))
-    fw = open('./trainval_data.txt', 'wb')
+    fw = open('./msrvtt_trainval_data.txt', 'wb')
     for video_name in videos:
-        video_path = os.path.join(root_path, 'YouTubeClips', video_name)
+        video_path = os.path.join(root_path, 'TrainValVideo', video_name)
         # feat_path = os.path.join(root_path, 'features', video_name.replace('.avi','.npy'))
         # line = '{} {}\n'.format(video_path, feat_path)
         line = '{}\n'.format(video_path)
