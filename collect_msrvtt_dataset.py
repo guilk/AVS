0421129ce639c175882023e895dac31d03e81e1b
guilk <guiliangke@gmail.com>
2019-07-06 16:31:38 -0400

    extract test videos

--- collect_msrvtt_dataset.py
+++ collect_msrvtt_dataset.py
@@ -5,10 +5,10 @@ import random
 if __name__ == '__main__':
 
     root_path = '/mnt/sda/AVS/MSR-VTT/'
-    videos = os.listdir(os.path.join(root_path, 'TrainValVideo'))
-    fw = open('./msrvtt_trainval_data.txt', 'wb')
+    videos = os.listdir(os.path.join(root_path, 'TestVideo'))
+    fw = open('./msrvtt_test_data.txt', 'wb')
     for video_name in videos:
-        video_path = os.path.join(root_path, 'TrainValVideo', video_name)
+        video_path = os.path.join(root_path, 'TestVideo', video_name)
         # feat_path = os.path.join(root_path, 'features', video_name.replace('.avi','.npy'))
         # line = '{} {}\n'.format(video_path, feat_path)
         line = '{}\n'.format(video_path)
